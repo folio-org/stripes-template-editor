folio-org/stripes-template-editor

commit 04ce17c04f89ef56a3cde391cd1c318c53d86744

[Fix] use different id for each editor (#19)
Use different id for each editor. Previously, the component used a specific id for the editor toolbar, preventing multiple editors from existing at the same time.

src/TemplateEditor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactQuill, { Quill } from 'react-quill';
+import { v4 as uuidv4 } from 'uuid';
 
 import {
   isNull,
@@ -64,10 +65,12 @@ class TemplateEditor extends React.Component {
     Quill.register('formats/indent', IndentStyle, true);
 
     this.quill = React.createRef();
+    this.quillId = uuidv4();
+    this.quillToolbarId = uuidv4();
 
     this.modules = {
       toolbar: {
-        container: '#toolbar',
+        container: `#${this.quillToolbarId}`,
         handlers: {
           token: this.openTokenDialog,
         }
@@ -221,9 +224,9 @@ class TemplateEditor extends React.Component {
             <Row>
               <Col xs={12}>
                 <div {... invalid ? { className: css.error } : {}}>
-                  <EditorToolbar />
+                  <EditorToolbar id={this.quillToolbarId} />
                   <ReactQuill
-                    id="template-editor"
+                    id={this.quillId}
                     className={css.editor}
                     value={value}
                     ref={this.quill}

src/EditorToolbar/EditorToolbar.js
@@ -5,12 +5,13 @@ import {
 } from 'react-intl';
 
 const EditorToolbar = ({
+  id,
   intl: {
     formatMessage,
   }
 }) => {
   return (
-    <div id="toolbar">
+    <div id={id}>
       <span className="ql-formats">
         <button
           type="button"
@@ -128,6 +129,7 @@ const EditorToolbar = ({
 };
 
 EditorToolbar.propTypes = {
+  id: PropTypes.string,
   intl: PropTypes.object,
 };
 

commit 1add9b1ffcd0289cf2fa4a8dac142895a36f2e61

STRIPES-832 use alpha-prefixes for HTML id attributes
Using a naked UUID as an html `id` attribute will cause problems when
trying to retrieve elements using CSS selectors because they can start
with 0-9 and a-f but a CSS selector can't start with a number. IOW, even
though an HTML `id` is technically allowed to start with and contain any
character, in order to play nice with CSS selectors they need to start
with a letter. And now these two do.

Fixes the bug introduced by #19.

Refs STRIPES-832

src/TemplateEditor.js
@@ -65,8 +65,8 @@ class TemplateEditor extends React.Component {
     Quill.register('formats/indent', IndentStyle, true);
 
     this.quill = React.createRef();
-    this.quillId = uuidv4();
-    this.quillToolbarId = uuidv4();
+    this.quillId = `rte-${uuidv4()}`;
+    this.quillToolbarId = `rte-toolbar-${uuidv4()}`;
 
     this.modules = {
       toolbar: {