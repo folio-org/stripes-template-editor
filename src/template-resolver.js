64cc68da7abdd5de2edfd6ae525056b8522d39cf
Zak Burke <zburke@ebsco.com>
2023-09-20 13:18:02 -0400

    STRIPES-886 support React v18 (#51)
    Support React v17 and v18. Update related deps as well.
    
    Refs STRIPES-886

--- src/template-resolver.js
+++ src/template-resolver.js
@@ -1,8 +1,9 @@
-export default function (templateStr) {
+export default (templateStr) => {
   return (tokensList) => {
     return templateStr.replace(/{{([^{}]*)}}/g, (tag, tokenName) => {
       const tokenValue = tokensList[tokenName];
       return typeof tokenValue === 'string' || typeof tokenValue === 'number' ? tokenValue : '';
     });
   };
-}
+};
+
