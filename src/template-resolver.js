c411783895475eda48a45395dfc8d7a22dbbd626
Zak Burke <zburke@ebsco.com>
2024-03-14 11:03:41 -0400

    STRIPES-908 use DOMPurify to avoid XSS (#58)
    Push content through DOMPurify to avoid XSS vulnerabilities.
    
    Also fix old lint errors. Sorry, should've done that separately.
    
    Refs STRIPES-908, UICIRC-1060

--- src/template-resolver.js
+++ src/template-resolver.js
@@ -1,9 +1,19 @@
+/**
+ * templateResolver
+ * given a string containing placeholders like {{foo}}, return a function
+ * that takes a map of key-value pairs and replaces the placeholders
+ * in the template-string with values from the map if they are strings
+ * or numbers; placeholders without a corresponding key and keys with
+ * other types of values are replaced with an empty string.
+ *
+ * @argument {string} templateStr
+ * @returns {function} that takes a map and returns a strng
+ */
 export default (templateStr) => {
   return (tokensList) => {
-    return templateStr.replace(/{{([^{}]*)}}/g, (tag, tokenName) => {
+    return templateStr.replace(/{{([^{}]*)}}/g, (_tag, tokenName) => {
       const tokenValue = tokensList[tokenName];
       return typeof tokenValue === 'string' || typeof tokenValue === 'number' ? tokenValue : '';
     });
   };
 };
-
